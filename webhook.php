044e5e7bc74b514c7ca1792af9babef37266bc12
Andrew Afonso <andrewbluepiano@hotmail.com>
2020-01-28 13:25:44 -0500

    Pretty emails
    Much more readable emails.

--- webhook.php
+++ webhook.php
@@ -17,10 +17,24 @@ $output = shell_exec($command);
 // Constructs and sends the alert email
 if($output == 1){
     $email_subject = "Priority Motion Alert";
-    $email_body = serialize($webhook);
-    $headers = "From: YOURSERVER@noreply.com \r\n";
-    $headers .= "Reply-To: YOURSERVER@noreply.com \r\n";
-    mail($myEmail,$email_subject,$email_body,$headers);
+    $headers = "From: MVAlert@noreply.com \r\n";
+    $headers .= "Reply-To: MVAlert@noreply.com \r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    // Checks if just someone loading the page.
+    if($data != "N;"){
+        $email_body = "<h1>Motion Alert</h1>";
+        $email_body .= "<h3>Org Info</h3>";
+        $email_body .=  $webhook['organizationName'] . "<br>";
+        $email_body .=  $webhook['organizationUrl'] . "<br><br>";
+        $email_body .= "<h3>Camera Info</h3>";
+        $email_body .=  $webhook['deviceName'] . " : " . $webhook['deviceModel'] . " : " . $webhook['deviceMac'] . "<br>";
+        $email_body .=  $webhook['deviceUrl'] . "<br><br>";
+        $email_body .= "<h3>Alert Info</h3>";
+        $email_body .= '<img src="'. $webhook['alertData']['imageUrl'] .'" alt="Snapshot" />';
+        $email_body .=  $webhook['alertData']['timestamp'] . "<br>";
+        mail($myEmail,$email_subject,$email_body,$headers);
+    }
     error_log(serialize($webhook), 0);
 }
 ?>
