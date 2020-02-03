968741b984cd9f2c20b548a0b969f30fa6cd5499
Andrew Afonso <andrewbluepiano@hotmail.com>
2020-02-02 21:38:54 -0500

    Update webhook.php
    Added in link to specific timestamp in footage for camera.
    
    Added in snapshots for cameras using Motion Detection 2.0
    
    Using the normal Dashboard Snapshot API to get snapshots for MVx1's was attempted, and its way too finnicky.

--- webhook.php
+++ webhook.php
@@ -3,14 +3,14 @@
 // GitHub: https://github.com/andrewbluepiano/SmarterMVMotionWebhookAlerts
 
 // Set $myEmail to the email you would like alerts delivered to, and $sharedSecret to the shared secret you enter in dashboard
-$myEmail = "YOUR EMAIL";
-$sharedSecret = "ABCDE";
+$myEmail = "YOUR EMAIL HERE";
+$sharedSecret = "SOMEsecret";
 
 // Receives and decodes the message from dashboard
 $body = file_get_contents("php://input");
 $webhook = json_decode($body, true);
 
-// Check
+// Check shared secret
 if($webhook['sharedSecret'] != $sharedSecret){
     die("Error");
 }
@@ -23,13 +23,15 @@ $output = shell_exec($command);
 
 // Constructs and sends the alert email
 if($output == 1){
+    // Sets Email headers
     $email_subject = "Priority Motion Alert";
     $headers = "From: MVAlert@noreply.com \r\n";
     $headers .= "Reply-To: MVAlert@noreply.com \r\n";
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/html; charset=utf-8\r\n";
+    
     // Checks if just someone loading the page.
-    if($data != "N;"){
+    if($webhook != "N;"){
         $email_body = "<html><body>";
         
         $email_body .= "<h1>Motion Alert</h1>";
@@ -39,7 +41,8 @@ if($output == 1){
         $email_body .=  $webhook['organizationUrl'] . "<br><br>";
         
         $email_body .= "<h3>Camera Info</h3>";
-        if($webhook['deviceName'] != ""){
+        
+        if($webhook['deviceName'] != ""){    // If device has a custom name set in dashboard
             $email_body .=  $webhook['deviceName'] . " : ";
         }
         $email_body .= $webhook['deviceModel'] . " : " . $webhook['deviceMac'] . "<br>";
@@ -47,13 +50,19 @@ if($output == 1){
         
         $email_body .= "<h3>Alert Data</h3>";
         $email_body .=  "Timestamp: " . date( "Y-m-d T H:i:s", $webhook['alertData']['timestamp']) . "<br>";
-        $email_body .= "Image URL: " . $webhook['alertData']['imageUrl'] . "<br>";
-//        $email_body .= '<img src="'. $webhook['alertData']['imageUrl'] .'" alt="Snapshot" width="500" height="281" /><br>';
+        $email_body .=  "Review footage: <a href=\"" . $webhook['deviceUrl'] . "?timestamp=" . str_replace( ".", "", $webhook['alertData']['timestamp']) . ".0\">Here</a><br>";
         
+        // Only embed image if the device is using Motion Detection 2.0
+        if($webhook['deviceModel'] != "MV71" && $webhook['deviceModel'] != "MV21" && $webhook['alertData']['imageEnabled']){
+            $email_body .= '<img src="'. $webhook['alertData']['imageUrl'] .'" alt="Snapshot" width="500" height="281" /><br>';
+        }
+       
         $email_body .= "</body></html>";
         
+        // Send the email.
         mail($myEmail,$email_subject,$email_body,$headers);
     }
+    
     error_log(serialize($webhook), 0);
 }
 ?>
